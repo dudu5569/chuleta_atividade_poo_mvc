667cd63d4586bc652075a1d0982d06001c01ffc9
Duh <wakaw5302@gmail.com>
2024-10-10 08:32:10 -0300

    first commit

---
+++ conn/connect.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $database = "tincphp01";
+    $user = "root";
+    $pass = "";
+    $charset = "utf8";
+    $port = "3306";
+
+    try {
+        $conn = new mysqli($host, $user, $pass, $database, $port);
+        mysqli_set_charset($conn,$charset);
+    }catch(trhowable $th){
+        die("Atenção rolou um ERRO, cara! ".$th);
+    }
